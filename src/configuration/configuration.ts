b464482813a988f62361c28ac12c4ca8794e5b2a
DiegoVega19 <davila9923@gmail.com>
2023-11-23 21:49:52 -0600

    mejoras a los servicios

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
 
-const YAML_CONFIG_FILENAME = `config/${process.env.CLIENT_INSTANCE}.yaml;`
+const YAML_CONFIG_FILENAME = `config/${process.env.CLIENT_INSTANCE}.yaml`
 const back = join(__dirname,'../');
 export default () => {
   return yaml.load(
